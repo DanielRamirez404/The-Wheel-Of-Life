9af079cda70edcc83b235189751deaa41801f9e5
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-04-09 01:18:51 -0400

    Add cone shape

--- Wheel-Of-Life/src/components/WheelOfLife.vue
+++ Wheel-Of-Life/src/components/WheelOfLife.vue
@@ -1,6 +1,9 @@
 <template>
-    <h1>Hi, {{ name }}!</h1>
-    <span class="dot"></span>
+    <h1>Wheel of Life</h1>
+    <h3>Hi, {{ name }}!</h3>
+    <span class="dot">
+        <span class="cone"></span>
+    </span>
 </template>
 
 <script setup lang="ts">
@@ -8,11 +11,27 @@ defineProps<{ name: string }>()
 </script>
 
 <style scoped>
+
 .dot {
-      height: 250px;
-      width: 250px;
-      background-color: #bbb;
-      border-radius: 50%;
-      display: inline-block;
+    height: 250px;
+    width: 250px;
+    background-color: #bbb;
+    border-radius: 50%;
+    display: inline-block;
 }
+
+.cone {
+    
+    position: absolute;
+
+    width: 0;
+    height: 0;
+    border-left: 70px solid transparent;
+    border-right: 70px solid transparent;
+    border-top: 100px solid gray;
+    border-radius: 50%;
+
+    transform: rotate(180deg);
+}
+
 </style>
