dcebdf6fecb012c8e0589e77046d37a19f3fb7a0
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-04-09 00:54:56 -0400

    Add preliminary version of the WheelOfLife component

---
+++ Wheel-Of-Life/src/components/WheelOfLife.vue
@@ -0,0 +1,18 @@
+<template>
+    <h1>Hi, {{ name }}!</h1>
+    <span class="dot"></span>
+</template>
+
+<script setup lang="ts">
+defineProps<{ name: string }>()
+</script>
+
+<style scoped>
+.dot {
+      height: 250px;
+      width: 250px;
+      background-color: #bbb;
+      border-radius: 50%;
+      display: inline-block;
+}
+</style>
