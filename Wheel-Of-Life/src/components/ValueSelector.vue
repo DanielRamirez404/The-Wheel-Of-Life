c41b0a83c5c289688809c4af9d34df02000c324c
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-04 20:44:51 -0400

    Add the ValueSelector component

---
+++ Wheel-Of-Life/src/components/ValueSelector.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import ValueButton from './ValueButton.vue'
+
+const props = defineProps<{ onClick: (number) => void }>();
+</script>
+
+<template>
+    <ValueButton v-for="i in 10" :key='i' :value='i' :onClick="props.onClick" /> 
+</template>
+
+<style scoped>
+</style>
