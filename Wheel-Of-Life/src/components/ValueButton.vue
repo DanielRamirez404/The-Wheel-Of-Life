27f90070033dd6356f8f27bbccff38dfda441cd2
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-04 20:44:24 -0400

    Add the ValueButton component

---
+++ Wheel-Of-Life/src/components/ValueButton.vue
@@ -0,0 +1,39 @@
+<script setup lang="ts">
+const props = defineProps<{ value: int, onClick: (number) => void }>();
+</script>
+
+<template>
+    <button @click="props.onClick(props.value)">{{props.value}}</button>
+</template>
+
+<style scoped>
+button {
+    width: 75px;
+    height: 75px;
+
+    background: white;
+    border: none;
+    margin: 10px;
+    padding: 5px;
+    
+    font-family: "Cal Sans", sans-serif;
+    font-weight: 400;
+    font-style: normal;    
+    text-align: center;
+    color: black;
+    font-size: 50px;
+
+    cursor: pointer;
+    outline: none;
+    
+    transition: background 0.15s;
+}
+
+button:hover {
+    background: #f0f0f0;
+}
+
+button:active {
+    background: #e0e0e0;
+}
+</style>
