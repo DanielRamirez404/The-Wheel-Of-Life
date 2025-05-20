686e6961f86d5f23134c4350169f2017ac0997c5
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-20 10:08:32 -0400

    Delete comment

--- Wheel-Of-Life/src/router.ts
+++ Wheel-Of-Life/src/router.ts
@@ -3,7 +3,6 @@ import LifeCanvas from './components/LifeCanvas.vue'
 import FieldsSelector from './components/FieldsSelector.vue'
 
 const routes = [
-    //{ path: '/:catchAll(.*)', redirect: '/Wheel-Of-Life' },
     { path: '/Wheel-Of-Life/wheel', name: 'Canvas', component: LifeCanvas },
     { path: '/Wheel-Of-Life/', name: 'Selección', component: FieldsSelector },
 ];
