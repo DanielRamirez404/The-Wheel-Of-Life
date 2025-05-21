f587bc71ced5d28fa3daf095de9a74eab7d708d1
DanielRamirez404 <danielramirezabou404@gmail.com>
2025-05-21 05:02:15 -0400

    Change vite build configuration

--- Wheel-Of-Life/vite.config.ts
+++ Wheel-Of-Life/vite.config.ts
@@ -5,4 +5,14 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   base: '/Wheel-Of-Life/',
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      output:{
+        manualChunks(id) {
+          if (id.includes('node_modules'))
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+        }
+      }
+    }
+  },
 })
